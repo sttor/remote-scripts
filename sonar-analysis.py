375935fb88b4e9975687e6958779a5ec6e489fdc
Raushan Raj <54439583+sttor@users.noreply.github.com>
2022-11-21 19:34:08 +0530

    Create sonar-analysis.py

---
+++ sonar-analysis.py
@@ -0,0 +1,45 @@
+import requests, json, os, time, sys
+from lxml.html import fromstring
+
+
+class SonarQubeReportSlack:
+
+    def __init__(self):
+        self.component = os.getenv("component")
+        self.sonar_url = os.getenv("sonar_url")
+        self.sonar_token = os.getenv("sonar_token")
+
+
+    def generate_summary_and_report(self):
+        cmd = """sonar-report  --sonarurl="%s" --sonartoken="%s"  --sonarcomponent="%s" --allbugs="false" > sonar_report.html """
+        cmd = cmd % (self.sonar_url, self.sonar_token, self.component)
+        os.system(cmd)
+        with open('sonar_report.html') as f: report = f.read()
+        count, summary, summarytable = self.generate_summary(report)
+        print("::set-output name=summarytable::%s" % summarytable)
+        print("::set-output name=summary::%s" % summary)
+
+    def generate_summary(self, report):
+        html_str = fromstring(report)
+        issues = html_str.xpath("//div[@class='summup']//tr/td/text()")
+        isitr = iter(issues)
+        issues_dict = dict(zip(isitr, isitr))
+        summary_table = self.get_summary_table(issues_dict)
+        count = int(issues_dict.get("BLOCKER", 0)) + int(issues_dict.get("CRITICAL", 0))
+        return count, "SAST %s: %s Blocker/Critical Issues Identified in the Repository" % (
+        self.component, str(count)), summary_table
+
+    def get_summary_table(self, issues_dict):
+        return "| Severity | Number of Issues |%0A| --- | --- |%0A| BLOCKER | {blocker} " \
+               "  |%0A| CRITICAL | {critical}   |%0A| MAJOR | {major} " \
+               " |%0A| MINOR | {minor}  |".format(blocker=issues_dict.get("BLOCKER", "0"),
+                                                  critical=issues_dict.get("CRITICAL", "0"),
+                                                  major=issues_dict.get("MAJOR", "0"),
+                                                  minor=issues_dict.get("MINOR", "0"))
+
+    def run(self):
+        self.wait_for_analysis()
+        self.generate_summary_and_report()
+
+
+SonarQubeReportSlack().run()
