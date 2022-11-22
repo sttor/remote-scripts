3c3539203def3261e5efacedfc73b8889a297f6a
Raushan Raj <54439583+sttor@users.noreply.github.com>
2022-11-22 08:47:32 +0530

    Update sonar-analysis.py

--- sonar-analysis.py
+++ sonar-analysis.py
@@ -8,7 +8,33 @@ class SonarQubeReportSlack:
         self.component = os.getenv("PROJECT_KEY")
         self.sonar_url = os.getenv("SONAR_HOST_URL")
         self.sonar_token = os.getenv("SONAR_USER_TOKEN")
+        self.fail_build = os.getenv("FAIL", "false")
 
+    def wait_for_analysis(self):
+        ATTEMPTS = 10
+        url = self.sonar_url + "/api/ce/component?component=%s" % (self.component,)
+        while True:
+            res = requests.get(url, auth=(self.sonar_token, "")).json()
+            if "current" in res.keys():
+                try:
+                    self.analysis_id = res["current"]["analysisId"]
+                except Exception as e:
+                    print("Analysis Id key error")
+            if "queue" not in res.keys() or not res["queue"] or ATTEMPTS == 0:
+                break
+            time.sleep(10)
+            ATTEMPTS -= 1
+
+    def quality_gate_status(self):
+        status = "PASS"
+        try:
+            url = self.sonar_url + "/api/qualitygates/project_status?analysisId=" + self.analysis_id
+            res = requests.get(url, auth=(self.sonar_token,"")).json()
+            status = res["projectStatus"]["status"]
+            print("::set-output name=qualitygate::%s" % "SonarQube Quality Gate Status is : "+status)
+        except Exception as e:
+            print("Error with fetching quality gates")
+        return status
 
     def generate_summary_and_report(self):
         cmd = """sonar-report  --sonarurl="%s" --sonartoken="%s"  --sonarcomponent="%s" """
@@ -18,6 +44,10 @@ class SonarQubeReportSlack:
         count, summary, summarytable = self.generate_summary(report)
         print("::set-output name=summarytable::%s" % summarytable)
         print("::set-output name=summary::%s" % summary)
+        status = self.quality_gate_status()
+        if status == "ERROR" and self.fail_build == "true":
+            print("Quality gate failed, check on sonarqube)"
+            sys.exit(1)
 
     def generate_summary(self, report):
         html_str = fromstring(report)
@@ -27,7 +57,7 @@ class SonarQubeReportSlack:
         summary_table = self.get_summary_table(issues_dict)
         count = int(issues_dict.get("BLOCKER", 0)) + int(issues_dict.get("CRITICAL", 0))
         return count, "SAST %s: %s Blocker/Critical Issues Identified in the Repository" % (
-        self.component, str(count)), summary_table
+            self.component, str(count)), summary_table
 
     def get_summary_table(self, issues_dict):
         return "| Severity | Number of Issues |%0A| --- | --- |%0A| BLOCKER | {blocker} " \
@@ -38,6 +68,7 @@ class SonarQubeReportSlack:
                                                   minor=issues_dict.get("MINOR", "0"))
 
     def run(self):
+        self.wait_for_analysis()
         self.generate_summary_and_report()
 
 
